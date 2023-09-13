1f498cdb538f5d07bcdcb87816bbc4688e766047
Juan Escobar <juanjv.escobar@gmail.com>
2023-09-13 20:14:26 +0200

    Subir prueba con dynamo

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict'
 
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'eu-north-1', // Reemplaza con tu región de DynamoDB
+  accessKeyId: 'AKIAYNILH2AIE3CJMGF2',
+  secretAccessKey: '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'
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+const params = {
+  TableName: 'proyectos', // Reemplaza con el nombre de tu tabla
+};
+
+dynamodb.scan(params, (error, data) => {
+  if (error) {
+    console.error('Error al escanear la tabla:', error);
+  } else {
+    console.log('Elementos en la tabla:');
+    data.Items.forEach(item => {
+      console.log(item);
+    });
+  }
+});
+
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3700;
